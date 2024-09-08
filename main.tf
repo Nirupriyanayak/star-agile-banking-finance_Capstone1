6db6c40f8ba77bd625f73922aaa1a84b0cc08c5f
Niru.nayak <35395338+Nirupriyanayak@users.noreply.github.com>
2024-09-08 21:51:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ provider "aws" {
 }
 resource "aws_key_pair" "example" {
   key_name = "key02"
-  public_key = file("~/.ssh/id_ed25519.pub")
+  public_key = file("/var/lib/jenkins/.ssh/id_ed25519.pub")
 }
 
 resource "aws_instance" "server" {
